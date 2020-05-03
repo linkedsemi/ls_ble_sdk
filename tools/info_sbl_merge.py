03bd50e01fe1ead1dc38abca8cd39117a55e1ecf
zxtxin <zxtxin@126.com>
2020-05-03 12:32:10 +0800

    public addr add to info merge script
    Test Plan: No
    
    Reviewers: jwzhang, mingzhou
    
    Differential Revision: http://10.10.20.8/D81

--- tools/info_sbl_merge.py
+++ tools/info_sbl_merge.py
@@ -6,20 +6,24 @@ import struct
 fh_i = open(sys.argv[1],'rb')
 flash_base = int(sys.argv[2],16)
 sbl_data = fh_i.read()
+public_mac_addr_base = flash_base + 0x20
 
 test_word0 = 0xa5a53c3c
 test_word1 = 0x5a5ac3c3
-cap_delay = 4
+cap_delay = 2
 spi_clk = 0
 cfg_info = 0xffffffff
 image_crypt = 0xffffffff
 sbl_code_start = flash_base+0x1000
 sbl_code_length = len(sbl_data)
 
+public_addr = bytes([0x20,0x20,0x05,0x03,0x50,0x10])
+
 info_head = struct.pack('LLHHLLLL',test_word0,test_word1,cap_delay,spi_clk,cfg_info,image_crypt,sbl_code_start,sbl_code_length)
 
 ih = IntelHex()
 ih.puts(flash_base,info_head)
+ih.puts(public_mac_addr_base,public_addr)
 ih.puts(sbl_code_start,sbl_data)
 ih.write_hex_file(sys.argv[3])
 
