067a97dea025907c76d3b4d11d9580f69b5cdaed
mingzhou <mingzhou@linkedsemi.com>
2020-05-19 17:23:28 +0800

    Modify disassemble.bat for sbl.
    Test Plan: No
    
    Reviewers: jwzhang, xtzhang
    
    Reviewed By: xtzhang
    
    Differential Revision: http://10.10.20.8/D135

--- tools/disassemble.bat
+++ tools/disassemble.bat
@@ -1,2 +1,8 @@
-@fromelf --i32 --output=.\UVBuild\%1.hex .\UVBuild\%1.axf
-@fromelf -c -a -d -e -v -o .\UVBuild\%1.asm .\UVBuild\%1.axf
+@echo OFF
+if "%1"=="sbl" ( 
+ @fromelf --bin --output=..\..\..\..\build\examples\%1.bin .\UVBuild\%1.axf
+ @python ..\..\..\..\tools\info_sbl_merge.py ..\..\..\..\build\examples\%1.bin 0x18000000 ..\..\..\..\build\examples\info_sbl.hex
+) else (
+ @fromelf --i32 --output=.\UVBuild\%1.hex .\UVBuild\%1.axf
+ @fromelf -c -a -d -e -v -o .\UVBuild\%1.asm .\UVBuild\%1.axf
+)
