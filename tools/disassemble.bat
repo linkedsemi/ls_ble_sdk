789b9e8bca23c65364379eb9592fb59167536e7f
mingzhou <mingzhou@linkedsemi.com>
2020-05-28 18:33:02 +0800

    Modify bat for sbl.
    Test Plan: no
    
    Reviewers: jwzhang, xtzhang
    
    Reviewed By: xtzhang
    
    Differential Revision: http://10.10.20.8/D181

--- tools/disassemble.bat
+++ tools/disassemble.bat
@@ -1,8 +1,8 @@
 @echo OFF
 if "%1"=="sbl" ( 
- @fromelf --bin --output=..\..\..\..\build\examples\%1.bin .\UVBuild\%1.axf
- @python ..\..\..\..\tools\info_sbl_merge.py ..\..\..\..\build\examples\%1.bin 0x18000000 ..\..\..\..\build\examples\info_sbl.hex
+echo fromelf --bin --output=.\UVBuild\%1.bin .\UVBuild\%1.axf
+echo python ..\..\..\..\tools\info_sbl_merge.py .\UVBuild\%1.bin 0x18000000 .\UVBuild\info_sbl.hex
 ) else (
- @fromelf --i32 --output=.\UVBuild\%1.hex .\UVBuild\%1.axf
- @fromelf -c -a -d -e -v -o .\UVBuild\%1.asm .\UVBuild\%1.axf
+echo fromelf --i32 --output=.\UVBuild\%1.hex .\UVBuild\%1.axf
+echo fromelf -c -a -d -e -v -o .\UVBuild\%1.asm .\UVBuild\%1.axf
 )
