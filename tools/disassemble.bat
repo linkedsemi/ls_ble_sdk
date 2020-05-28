43cf75f027aab9f12a6687a239517449583211f4
yanluhua <yanluhua@forever-tech.net>
2020-05-28 21:11:20 +0800

    modify armcc link txt
    Test Plan: No
    
    Reviewers: jwzhang, mingzhou, xtzhang
    
    Reviewed By: mingzhou
    
    Differential Revision: http://10.10.20.8/D183

--- tools/disassemble.bat
+++ tools/disassemble.bat
@@ -1,8 +1,12 @@
 @echo OFF
 if "%1"=="sbl" ( 
 echo fromelf --bin --output=.\UVBuild\%1.bin .\UVBuild\%1.axf
+ fromelf --bin --output=.\UVBuild\%1.bin .\UVBuild\%1.axf
 echo python ..\..\..\..\tools\info_sbl_merge.py .\UVBuild\%1.bin 0x18000000 .\UVBuild\info_sbl.hex
+ python ..\..\..\..\tools\info_sbl_merge.py .\UVBuild\%1.bin 0x18000000 .\UVBuild\info_sbl.hex
 ) else (
 echo fromelf --i32 --output=.\UVBuild\%1.hex .\UVBuild\%1.axf
+ fromelf --i32 --output=.\UVBuild\%1.hex .\UVBuild\%1.axf
 echo fromelf -c -a -d -e -v -o .\UVBuild\%1.asm .\UVBuild\%1.axf
+ fromelf -c -a -d -e -v -o .\UVBuild\%1.asm .\UVBuild\%1.axf
 )
