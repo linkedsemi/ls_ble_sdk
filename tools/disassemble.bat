ac88a78fcbbcb64bb02ce7c4f820fc9934fb10e3
mingzhou <mingzhou@linkedsemi.com>
2020-09-11 17:07:32 +0800

    Fix bug in disassemble.bat
    Test Plan: no
    
    Reviewers: jwzhang, xtzhang
    
    Reviewed By: xtzhang
    
    Differential Revision: http://10.10.20.8/D399

--- tools/disassemble.bat
+++ tools/disassemble.bat
@@ -5,6 +5,6 @@ echo fromelf --i32 --output=.\UVBuild\%1.hex .\UVBuild\%1.axf
  fromelf --i32 --output=.\UVBuild\%1.hex .\UVBuild\%1.axf
 echo fromelf -c -a -d -e -v -o .\UVBuild\%1.asm .\UVBuild\%1.axf
  fromelf -c -a -d -e -v -o .\UVBuild\%1.asm .\UVBuild\%1.axf
-echo hexmerge.py .\UVBuild\info_sbl.hex %2\dev\soc\arm_cm\le501x\bin\fw.hex .\UVBuild\ble_uart_server.hex .\UVBuild\%1_production_%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%.hex
- hexmerge.py .\UVBuild\info_sbl.hex %2\dev\soc\arm_cm\le501x\bin\fw.hex .\UVBuild\ble_uart_server.hex > .\UVBuild\%1_production_%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%.hex
+echo hexmerge.py .\UVBuild\info_sbl.hex %2\dev\soc\arm_cm\le501x\bin\fw.hex .\UVBuild\%1.hex .\UVBuild\%1_production_%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%.hex
+ hexmerge.py .\UVBuild\info_sbl.hex %2\dev\soc\arm_cm\le501x\bin\fw.hex .\UVBuild\%1.hex > .\UVBuild\%1_production_%date:~0,4%%date:~5,2%%date:~8,2%_%time:~0,2%%time:~3,2%%time:~6,2%.hex
 
