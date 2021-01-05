eacd10db67845e28e3cac8d3516fa0e374f898f8
zxtxin <zxtxin@126.com>
2021-01-05 20:09:29 +0800

    ram_link bug fix & flash write protect test
    Test Plan: No
    
    Reviewers: jwzhang, mingzhou
    
    Reviewed By: jwzhang
    
    Differential Revision: http://10.10.20.8/D569

--- dev/examples/ram_test/main.c
+++ dev/examples/ram_test/main.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 uint8_t test_buf[1024];
 uint8_t dst[1024];
+uint8_t status[2];
+
 int main_flash()
 {
     __disable_irq();
@@ -13,14 +15,17 @@ int main_flash()
     spi_flash_software_reset();
  //   spi_flash_write_status_register(0);
     spi_flash_qe_status_read_and_set();
-    spi_flash_sector_erase(0x2000);
-    spi_flash_chip_erase();
-    for(uint32_t i=0;i<1024;i+=4)
-    {
-        *(uint32_t *)&test_buf[i] = rand();
-    }
-    spi_flash_quad_page_program(0,test_buf,256);
-    spi_flash_quad_io_read(0,dst,sizeof(dst));
+    spi_flash_read_status_register_0(&status[0]);
+    spi_flash_read_status_register_1(&status[1]);
+    spi_flash_write_status_register(status[1]<<8|status[0]&~4<<2);
+//    spi_flash_sector_erase(0x2000);
+//    spi_flash_chip_erase();
+//    for(uint32_t i=0;i<1024;i+=4)
+//    {
+//        *(uint32_t *)&test_buf[i] = rand();
+//    }
+//    spi_flash_quad_page_program(0,test_buf,256);
+//    spi_flash_quad_io_read(0,dst,sizeof(dst));
 //    spi_flash_xip_start();
 
 //    memcpy(test_buf,(void *)0x18002000,sizeof(test_buf));
@@ -57,6 +62,8 @@ static void rc24m_switch_to_pll64m()
 int main()
 {
  
-    rc24m_switch_to_pll64m();
+    //rc24m_switch_to_pll64m();
+    systick_start();
+    main_flash();
     while(1);
 }
