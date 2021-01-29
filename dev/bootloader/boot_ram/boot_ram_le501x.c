a7e41d73102d5bb8d55d65fe62ba00cb7ce489b8
yanluhua <yanluhua@forever-tech.net>
2021-01-29 20:08:20 +0800

    lp2/lp3 wkup state clr
    Test Plan: No
    
    Reviewers: jwzhang, mingzhou, xtzhang
    
    Reviewed By: xtzhang
    
    Differential Revision: http://10.10.20.8/D619

--- dev/bootloader/boot_ram/boot_ram_le501x.c
+++ dev/bootloader/boot_ram/boot_ram_le501x.c
@@ -292,6 +292,8 @@ void boot_ram_start(uint32_t exec_addr)
     disable_global_irq();
     switch_to_rc32k();
     clk_switch();
+    REG_FIELD_WR(SYSCFG->PMU_WKUP, SYSCFG_LP_WKUP_CLR,1);
+    DELAY_US(200);
     SYSCFG->PMU_PWR = 0;
     systick_start();
     spi_flash_drv_var_init(false,false);
