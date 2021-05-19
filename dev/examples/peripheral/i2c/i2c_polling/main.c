d69505d4d598371a917a37195b92c1f7a15bc583
mingzhou <mingzhou@linkedsemi.com>
2021-05-19 21:15:47 +0800

    dev/soc/examples/i2c: Modify demos for i2c
    Test Plan: no
    
    Reviewers: jwzhang, xtzhang
    
    Reviewed By: jwzhang
    
    Differential Revision: http://10.10.20.8/D818

--- dev/examples/peripheral/i2c/i2c_polling/main.c
+++ dev/examples/peripheral/i2c/i2c_polling/main.c
@@ -6,45 +6,16 @@
 #include <stdlib.h>
 #include "log.h"
 
-/** @addtogroup le501x_HAL_Examples
-  * @{
-  */
+#define I2C_ADDRESS 0x51			//EEPROM addr
 
-/** @addtogroup SPI_FullDuplex_ComPolling
-  * @{
-  */ 
-
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-#define MASTER_BOARD
-
-//#define IICMODE_IT
-
-#define I2C_ADDRESS 0x50			//EEPROM addr
-
-#ifdef IICMODE_IT
-enum
-{
-	start = 0,
-	txing,
-	txover,
-	txaddr,
-	addrover,
-    rxing,
-	rxover,
-};
-volatile uint8_t Com_Sta = start;
-#endif
-/* Private macro -------------------------------------------------------------*/
-/* Private variables ---------------------------------------------------------*/
 /* I2C handler declaration */
-I2C_HandleTypeDef I2cHandle;
+static I2C_HandleTypeDef I2cHandle;
 
 /* Buffer used for transmission */
-uint8_t aTxBuffer[12] = {0x00,0x10,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xAA,};
+static uint8_t aTxBuffer[12] = {0x00,0x10,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xAA,};
 
 /* Buffer used for reception */
-uint8_t aRxBuffer[10];
+static uint8_t aRxBuffer[10];
 
 /* Private function prototypes -----------------------------------------------*/
 static void Error_Handler(void);
@@ -59,17 +30,17 @@ int main(void)
 {
 	int ii;
   /* system init app     */
-  sys_init_app();
+  sys_init_none();
   __enable_irq();
   /* Configure the GPIO AF */
   /* SDA-------------PB12 */	
   /* SCL-------------PB13 */	
-	iic1_io_init( PB13, PB12);
+	iic1_io_init(PB13, PB12);
 
 	
   /*##-1- Configure the I2C peripheral ######################################*/
   I2cHandle.Instance             = I2C1;
-  I2cHandle.Init.ClockSpeed      = 100000;
+  I2cHandle.Init.ClockSpeed      = 1000000;
   I2cHandle.Init.OwnAddress1     = I2C_ADDRESS;
   I2cHandle.Init.AddressingMode  = I2C_ADDRESSINGMODE_7BIT;
   I2cHandle.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
@@ -88,8 +59,7 @@ int main(void)
   /*##-2- Start the transmission process #####################################*/  
   /* While the I2C in reception process, user can transmit data through 
      "aTxBuffer" buffer */
-  /* Timeout is set to 1S */
-#ifndef IICMODE_IT
+
   while(HAL_I2C_Master_Transmit(&I2cHandle, (uint16_t)I2C_ADDRESS, (uint8_t*)aTxBuffer, 12, 1000)!= HAL_OK)
   {
     /* Error_Handler() function is called when Timeout error occurs.
@@ -120,41 +90,15 @@ int main(void)
       Error_Handler();
     }
   }
-#else
-	Com_Sta = txing;
-    while(HAL_I2C_Master_Transmit_IT(&I2cHandle, (uint16_t)I2C_ADDRESS, (uint8_t*)aTxBuffer, 12)!= HAL_OK)
-    {
-            ;
-    }
-
-	while(Com_Sta == txing);
-	
-	for(ii=0;ii<100;ii++)
-		DELAY_US(500);
-
-	Com_Sta = txaddr;
-    while(HAL_I2C_Master_Transmit_IT(&I2cHandle, (uint16_t)I2C_ADDRESS, (uint8_t*)aTxBuffer, 2)!= HAL_OK)
-    {
-        ;
-    }
- 	while(Com_Sta == txaddr);
-	Com_Sta = rxing;
-    while(HAL_I2C_Master_Receive_IT(&I2cHandle, (uint16_t)I2C_ADDRESS, (uint8_t*)aRxBuffer, 10)!= HAL_OK)
-    {
-        ;
-    }
-	while(Com_Sta == rxing);	
-#endif
 	while( Buffercmp(aTxBuffer+2, aRxBuffer, 10))
 	{
 		;
 	}
 
-
+    LOG_I("success!!!");
   /* Infinite loop */
   while (1)
   {
-		//HAL_SPI_Transmit(&SpiHandle, (uint8_t*)aTxBuffer, 1, 100);
 		
   }
 }
@@ -194,16 +138,3 @@ static void Error_Handler(void)
 		;
   }
 }
-#ifdef IICMODE_IT
-void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c)
-{
-  if(Com_Sta == txing)
-     Com_Sta = txover;
-  if(Com_Sta == txaddr)
-     Com_Sta = addrover;
-}
- void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c)
-{	
-  Com_Sta = rxover;
-}
-#endif
